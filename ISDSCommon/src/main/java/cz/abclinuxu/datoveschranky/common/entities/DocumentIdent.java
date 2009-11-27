42bf6d60301ab7fdb88d0254c3be70553ba05e9b
Vaclav Rosecky <xrosecky@gmail.com>
2009-11-27 19:43:23 +0100

    documentation added

---
+++ ISDSCommon/src/main/java/cz/abclinuxu/datoveschranky/common/entities/DocumentIdent.java
@@ -0,0 +1,47 @@
+package cz.abclinuxu.datoveschranky.common.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * Nepovinná identifikace zprávy, číslo jednací + spisová značka.
+ * 
+ * @author xrosecky
+ */
+public class DocumentIdent implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String refNumber;
+    private String ident;
+
+    public DocumentIdent(String refNumber, String ident) {
+        this.refNumber = refNumber;
+        this.ident = ident;
+    }
+
+    /**
+     * Číslo jednací
+     */
+    public String getIdent() {
+        return ident;
+    }
+
+    public void setIdent(String ident) {
+        this.ident = ident;
+    }
+
+    /**
+     * Spisová značka
+     */
+    public String getRefNumber() {
+        return refNumber;
+    }
+
+    public void setRefNumber(String refNumber) {
+        this.refNumber = refNumber;
+    }
+    
+    
+    
+}
